d08fa4d1e313e8a6270ce769279bfe42ff29a60b
David Clince <clinced@tcd.ie>
2016-11-30 16:47:53 +0000

    Connected mongoDB, ignore mongo files
    Might still need a few little fixes

--- distFileSys/src/Lib.hs
+++ distFileSys/src/Lib.hs
@@ -1,15 +1,23 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE TemplateHaskell      #-}
+{-# LANGUAGE TypeOperators        #-}
+
 module Lib
     ( startApp
     ) where
 
+import           Control.Monad.Trans
 import           Data.Aeson
 import           Data.Aeson.TH
 import           Data.List                (sortBy)
 import           Data.Ord                 (comparing)
 import           Data.Time.Calendar
+import           Database.MongoDB         (Action, Document, Value, access,
+                                           close, connect, delete, exclude,
+                                           find, host, insertMany, master,
+                                           project, rest, select, sort, (=:))
 import           Network.Wai
 import           Network.Wai.Handler.Warp
 import           Servant
@@ -60,3 +68,10 @@ sortedById = sortById users
 
 sortById :: [User] -> [User]
 sortById = sortBy (comparing userId)
+
+
+
+runMongo dbName functionToRun = do
+    pipe <- connect (host "localhost")
+    e <- access pipe master dbName functionToRun
+    close pipe
