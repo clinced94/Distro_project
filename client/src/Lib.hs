305d169c6296d72f0af87f7358e03c21aac3d96c
David Clince <clinced@tcd.ie>
2016-12-18 18:23:20 +0000

    Started to add client, not finished

---
+++ client/src/Lib.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE DeriveAnyClass       #-}
+{-# LANGUAGE DeriveGeneric        #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE TemplateHaskell      #-}
+{-# LANGUAGE TypeOperators        #-}
+
+module Lib
+    ( startApp
+    ) where
+
+import           Control.Monad            (forever, when)
+import           Control.Monad.Trans      (liftIO)
+import           Data.Aeson
+import           Data.Aeson.TH
+import           Data.Proxy
+import           Data.String
+import           GHC.Generics
+import           Network.HTTP.Client      (defaultManagerSettings, newManager)
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Servant
+import           Servant.API
+import           Servant.Client
+import           System.Exit
+
+
+startApp :: IO ()
+startApp = forever $ do
+    l <- getLine
+    when (l == "login") $ do
+        putStrLn ""
+        putStrLn "Enter username: "
+    when (l == "logout") $ do
+        putStrLn ""
+        putStrLn "Goodbye"
+        exitSuccess
+
+
+data Token = Token
+    { token :: Int
+    } deriving (Show, Generic, FromJSON, ToJSON)
+
+
+login :: Maybe String -> ClientM Token
+
+type AuthAPI = "login" :> QueryParam "username" String :> Get '[JSON] Token
+
+authAPI :: Proxy AuthAPI
+authAPI = Proxy
+
+login = client authAPI
